4478cb171172df0ad58b384fc30702259de68523
Ivan Pivnev <pivnev-1998@mail.ru>
2023-02-10 20:07:10 +0300

    if else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Маша")
+{
+    Console.Write("Это же Маша! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+};
